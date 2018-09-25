1e5f9208f2e9b78f2fd2bf404924ceced258f7d0
MH.Movasaghi <mmghho77@gmail.com>
2018-09-25 16:17:12 +0330

    Add project001
    First Project

---
+++ Project001.cpp
@@ -0,0 +1,13 @@
+//Project001 
+//Programming Foundations - Shahed University - 971
+
+#include <iostream> //Importing an "iostream" Library
+
+using namespace std; //using namespace for "iostream" Library
+
+int main() // Creating Main Function of Project
+{
+	cout << "Hello World !"; //Printing Hello World on the Console Screen
+
+	return 0; //return 0 to compiler that means program is finished
+}
