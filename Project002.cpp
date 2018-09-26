80ede8297dad3148399d16ecd022361e4800c512
MH.Movasaghi <mmghho77@gmail.com>
2018-09-26 21:16:20 +0330

    Add Project002
    Sample input / output

---
+++ Project002.cpp
@@ -0,0 +1,18 @@
+//Project002
+//Programming Foundations - Shahed University - 971
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	int input_var = 0; //decleration an int variable 
+	
+	cout << " Please Enter a Number : "; //printing a text for user
+	
+	cin >> input_var; //getting number from user
+	
+	cout << "\n Your Number is " << input_var << endl; //printing a text with number that user enter it
+	
+	return 0;
+}
